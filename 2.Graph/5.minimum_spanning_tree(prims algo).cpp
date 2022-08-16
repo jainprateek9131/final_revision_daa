ec7483d54532c090daaf9e2540ab8a826b8837c7
jainprateek9131 <68363818+jainprateek9131@users.noreply.github.com>
2022-08-16 21:01:45 +0530

    Create 5.minimum_spanning_tree(prims algo).cpp

---
+++ 2.Graph/5.minimum_spanning_tree(prims algo).cpp
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+	public:
+	//Function to find sum of weights of edges of the Minimum Spanning Tree.
+    int spanningTree(int V, vector<vector<int>> adj[])
+    {
+        // code here
+        vector<bool> visited(V,false);
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        pq.push({0,0});
+        int ans=0;
+        while(!pq.empty()){
+            pair<int,int> t=pq.top();
+            pq.pop();
+            int u = t.second;
+            if(visited[u])continue;
+            ans += (t.first);
+            visited[u] = true;
+            for(auto it : adj[u]){
+                int v=it[0];
+                int wt=it[1];
+                if(visited[v]==false){
+                    pq.push({wt,v});
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--) {
+        int V, E;
+        cin >> V >> E;
+        vector<vector<int>> adj[V];
+        int i=0;
+        while (i++<E) {
+            int u, v, w;
+            cin >> u >> v >> w;
+            vector<int> t1,t2;
+            t1.push_back(v);
+            t1.push_back(w);
+            adj[u].push_back(t1);
+            t2.push_back(u);
+            t2.push_back(w);
+            adj[v].push_back(t2);
+        }
+        
+        Solution obj;
+    	cout << obj.spanningTree(V, adj) << "\n";
+    }
+
+    return 0;
+}
+
+
+// } Driver Code Ends
