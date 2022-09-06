95d3f68c90ed2e8021816a93925399c922d9fdba
jainprateek9131 <68363818+jainprateek9131@users.noreply.github.com>
2022-09-06 09:21:52 +0530

    Update 1.Search_and_Insert_and_delete.cpp

--- 4.Binary_Search_Tree/1.Search_and_Insert_and_delete.cpp
+++ 4.Binary_Search_Tree/1.Search_and_Insert_and_delete.cpp
@@ -4,3 +4,22 @@ bool search(struct Node* root, int x) {
     else if(root->data<x)search(root->right,x);
     else search(root->left,x);
 }
+
+Node* insert(Node* root, int key) {
+    // Your code here
+    //if(search(root,key))return root;
+    struct Node* node=root,*parent=NULL;
+    while(node!=NULL){
+        parent = node;
+        if(key == node->data){
+            return root;
+        }else if(key > node->data){
+            node=node->right;
+        }else if(key<node->data){
+            node = node->left;
+        }
+    }
+    if(parent->data<key)parent->right=new Node(key);
+    else parent->left=new Node(key);
+    return root;
+}
