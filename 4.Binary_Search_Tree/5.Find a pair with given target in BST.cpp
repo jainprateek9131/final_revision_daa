fafccfcc077be981a118ac9d29261135bc565b68
jainprateek9131 <68363818+jainprateek9131@users.noreply.github.com>
2022-09-07 11:56:18 +0530

    Update 5.Find a pair with given target in BST.cpp

--- 4.Binary_Search_Tree/5.Find a pair with given target in BST.cpp
+++ 4.Binary_Search_Tree/5.Find a pair with given target in BST.cpp
@@ -1,3 +1,30 @@
+Given a Binary Search Tree and a target sum. Check whether there's a pair of Nodes in the BST with value summing up to the target sum. 
+
+Example 1:
+
+Input:
+        2
+      /   \
+     1     3
+sum = 5
+Output: 1 
+Explanation: 
+Nodes with value 2 and 3 sum up to 5.
+Example 2:
+
+Input:
+           6
+          /    
+         5     
+        /
+       3 
+     /  \
+    1    4
+sum = 2
+Output: 0 
+Explanation: 
+There's no pair that sums up to 2.
+
 Traverse tree and store each node in an element, if target-current element is already present return 1.
 unordered_map <int,int> mp;
 int ans = 0;
@@ -17,5 +44,37 @@ set<int>s;
         if(s.find(target-root->data)!=s.end())return true;
         s.insert(root->data);
         if(isPairPresent(root->right,target))return true;
+        return false;
+    }
+optimal :This can be done without extra space given that we are allowed to modify the tree.
+
+Convert BST to sorted doubly linked list (via in order traversal) and then apply 2 sum.
+     Node * head=NULL,*prev=NULL;
+    void helper(Node*root){
+        if(root==NULL)return;
+        helper(root->left);
+        if(prev==NULL){
+            head = root;
+        }else{
+            prev->right=root;
+            root->left=prev;
+        }
+        prev=root;
+        helper(root->right);
+    }
+    int isPairPresent(struct Node *root, int target)
+    {
+        helper(root);
+        Node* h = head, *t= prev;
+        while(h->data<t->data){
+            if(h->data+t->data==target){
+                return true;
+            }
+            else if(h->data+t->data<target){
+                h=h->right;
+            }else if(h->data+t->data>target){
+                t=t->left;
+            }
+        }
         return false;
     }
