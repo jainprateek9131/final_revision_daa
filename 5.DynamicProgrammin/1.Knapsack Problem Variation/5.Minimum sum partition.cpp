148db9cbbbb63f47232b41207a18938bf9a79601
jainprateek9131 <68363818+jainprateek9131@users.noreply.github.com>
2022-09-08 15:05:00 +0530

    Update 5.Minimum sum partition.cpp

--- 5.DynamicProgrammin/1.Knapsack Problem Variation/5.Minimum sum partition.cpp
+++ 5.DynamicProgrammin/1.Knapsack Problem Variation/5.Minimum sum partition.cpp
@@ -14,3 +14,58 @@ Output: 3
 Explanation: 
 Subset1 = {1}, sum of Subset1 = 1
 Subset2 = {4}, sum of Subset2 = 4
+//here we cannot use the memoization approch because it does not cover all blocks of table
+  
+-10 -10 -10 -10 -10 -10 -10 -10 -10 -10 -10 -10 -10 -10 -10 -10 -10 -10 -10 -10 -10 -10 -10 
+-10 1 -10 -10 -10 -10 -10 -10 -10 -10 -10 -10 -10 -10 -10 -10 -10 -10 -10 -10 -10 -10 -10 -10 
+-10 -10 -10 -10 -10 -10 -10 1 -10 -10 -10 -10 -10 -10 -10 -10 -10 -10 -10 -10 -10 -10 -10 -10 
+-10 -10 -10 -10 -10 -10 -10 -10 -10 -10 -10 -10 1 -10 -10 -10 -10 -10 -10 -10 -10 -10 -10 -10 
+-10 -10 -10 -10 -10 -10 -10 -10 -10 -10 -10 -10 -10 -10 -10 -10 -10 -10 -10 -10 -10 -10 -10 1 
+23
+4
+1 6 5 11
+  //{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+
+  public:
+    bool helper(int n, int arr[], int W ,vector<vector<int>>&dp){
+        if(W==0)return 1;
+        if( n==0){
+            return 0;
+        }
+        if(dp[n][W]!=-10){
+           return dp[n][W];
+        }
+       if(W >= arr[n-1]){
+           return dp[n][W]=helper(n-1,arr,W-arr[n-1],dp)||
+                          helper(n-1,arr,W,dp);
+       }else if(W < arr[n-1]){
+           return dp[n][W]=helper(n-1,arr,W,dp);
+       }cout<<dp[n][W]<<" ";
+    }
+	int minDifference(int arr[], int n)  { 
+	    int sum =0;
+	    for(int i=0;i<n;i++)
+	    sum+=arr[i];
+	    vector<vector<int>>dp(n+1,vector<int>(sum+1,-10));
+	    
+	    helper(n,arr,sum,dp);
+	    cout<<endl;
+	    for(int i=0;i<=n;i++){
+	    for(int j=0;j<=sum;j++)
+	    cout<<dp[i][j]<<" ";cout<<endl;
+	    }
+	    
+	    for(int i=sum/2 ; i>=0; i--){
+	    if(dp[n][i]==1){
+	        return sum-2*i;
+	     }
+	    }
+	    return sum;
+	} 
+	
+};
