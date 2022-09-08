030530078c2cc508a8d7d897a9968cf5625aeda9
jainprateek9131 <68363818+jainprateek9131@users.noreply.github.com>
2022-09-08 16:00:07 +0530

    Update 5.Minimum sum partition.cpp

--- 5.DynamicProgrammin/1.Knapsack Problem Variation/5.Minimum sum partition.cpp
+++ 5.DynamicProgrammin/1.Knapsack Problem Variation/5.Minimum sum partition.cpp
@@ -69,3 +69,80 @@ class Solution{
 	} 
 	
 };
+//bottom up approch
+
+1
+4
+1 6 5 11
+	
+	
+1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 
+1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 
+1 1 0 0 0 0 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 
+1 1 0 0 0 1 1 1 0 0 0 1 1 0 0 0 0 0 0 0 0 0 0 0 
+1 1 0 0 0 1 1 1 0 0 0 1 1 0 0 0 1 1 1 0 0 0 1 1 
+	ans = 1
+	
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+
+  public:
+	int minDifference(int arr[], int n)  { 
+	    int sum = 0;
+	    for ( int i = 0 ; i < n ; i++ ) {
+	        sum += arr[i];
+	    }
+	    bool dp[n+1][sum+1];
+	    for(int i=0 ;i<=n ;i++){
+	        dp[i][0]=true;
+	    }
+	    for(int j=1 ;j<=sum;j++){
+	        dp[0][j] = false;
+	    }
+	    dp[0][0] = true;
+	    for(int i=1 ; i<=n ; i++){
+	        for(int j=1 ; j<=sum ;j++){
+	            if(arr[i-1]<=j){
+	                dp[i][j] = dp[i-1][j-arr[i-1]] || dp[i-1][j];
+	            }else{
+	                dp[i][j] = dp[i-1][j];
+	            }
+	        }
+	    }
+	    for(int i = sum/2 ; i>=0 ; i--){
+	        if(dp[n][i]==true)return sum-2*i;
+	    }
+	    return sum;
+	} 
+};
+
+
+//{ Driver Code Starts.
+int main() 
+{
+   
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        int a[n];
+        for(int i = 0; i < n; i++)
+        	cin >> a[i];
+
+       
+
+	    Solution ob;
+	    cout << ob.minDifference(a, n) << "\n";
+	     
+    }
+    return 0;
+}
+// } Driver Code Ends
