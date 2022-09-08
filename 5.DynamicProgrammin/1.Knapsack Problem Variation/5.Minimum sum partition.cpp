24b7fa0a7493945db546618af8172315cdc8aad4
jainprateek9131 <68363818+jainprateek9131@users.noreply.github.com>
2022-09-08 09:23:01 +0530

    Create 5.Minimum sum partition.cpp

---
+++ 5.DynamicProgrammin/1.Knapsack Problem Variation/5.Minimum sum partition.cpp
@@ -0,0 +1,16 @@
+Partition a set into two subsets such that the difference of subset sums is minimum:-
+Given an array arr of size N containing non-negative integers, the task is to divide it into two sets S1 and S2 such that the absolute difference between their
+sums is minimum and find the minimum difference
+
+Example: 
+Input:  arr[] = {1, 6, 11, 5} 
+Output: 1
+Explanation:
+Subset1 = {1, 5, 6}, sum of Subset1 = 12 
+Subset2 = {11}, sum of Subset2 = 11 
+  
+Input: N = 2, arr[] = {1, 4}
+Output: 3
+Explanation: 
+Subset1 = {1}, sum of Subset1 = 1
+Subset2 = {4}, sum of Subset2 = 4
